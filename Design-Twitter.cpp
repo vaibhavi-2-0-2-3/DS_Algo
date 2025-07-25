3ef2bddd36f2979835d9b17323f03ba569f4afc8
Vaibhavi Gaonkar <v.a.i.bhavi062024@gmail.com>
2025-07-26 00:28:01 +0530

    [355] [Time Beats: 44.82%] [Memory Beats: 97.04%] - LeetPush

---
+++ Design-Twitter.cpp
@@ -0,0 +1,57 @@
+class Twitter {
+    int timestamp;
+    unordered_map<int, vector<pair<int, int>>>tweetMap;
+    unordered_map<int, unordered_set<int>>followMap;
+
+public:
+    Twitter() {
+        timestamp = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweetMap[userId].push_back({timestamp, tweetId});
+        timestamp++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        for(auto &followeeId : followMap[userId]){
+            for(auto &tweet : tweetMap[followeeId]){
+                pq.push(tweet);
+                if (pq.size() > 10) pq.pop(); 
+            }
+        }
+
+        for(auto &tweet : tweetMap[userId]){
+            pq.push(tweet);
+            if(pq.size() > 10) pq.pop();
+        }
+
+        vector<int> res;
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        followMap[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
